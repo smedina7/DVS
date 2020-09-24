dd6ed26a202c7da0e46e449a41b9264b0861f845
Bianca Alvarez <bsalvarez@miners.utep.edu>
2020-09-23 22:42:24 -0400

    changes to layout

--- QTableWidget.py
+++ QTableWidget.py
@@ -26,6 +26,13 @@ class Window(QWidget):
  
  
         self.show()
+
+        #d = json.loads(data)
+   ##with open('/home/kali/DVS/parsed/pykeylogger/keypressData.JSON') as f:
+     ##  data = json.load(f)
+    ##d = json.loads(data)
+    ##keys = ["keypresses_id", "Content", "Classname", "Start"]
+
  
     def creatingTables(self):
         self.tableWidget = QTableWidget()
@@ -37,22 +44,22 @@ class Window(QWidget):
         self.tableWidget.setItem(0, 2 , QTableWidgetItem("Log"))
         self.tableWidget.setItem(0, 3 , QTableWidgetItem("Tag"))
  
-        self.tableWidget.setItem(1,0, QTableWidgetItem("Parwiz"))
-        self.tableWidget.setItem(1,1, QTableWidgetItem("parwiz@gmail.com"))
-        self.tableWidget.setItem(1,2, QTableWidgetItem("845845845"))
+        self.tableWidget.setItem(1,0, QTableWidgetItem(""))
+        self.tableWidget.setItem(1,1, QTableWidgetItem(""))
+        self.tableWidget.setItem(1,2, QTableWidgetItem(""))
         self.tableWidget.setColumnWidth(1, 200)
  
-        self.tableWidget.setItem(2, 0, QTableWidgetItem("Ahmad"))
-        self.tableWidget.setItem(2, 1, QTableWidgetItem("ahmad@gmail.com"))
-        self.tableWidget.setItem(2, 2, QTableWidgetItem("2232324"))
+        self.tableWidget.setItem(2, 0, QTableWidgetItem(""))
+        self.tableWidget.setItem(2, 1, QTableWidgetItem(""))
+        self.tableWidget.setItem(2, 2, QTableWidgetItem(""))
  
-        self.tableWidget.setItem(3, 0, QTableWidgetItem("John"))
-        self.tableWidget.setItem(3, 1, QTableWidgetItem("john@gmail.com"))
-        self.tableWidget.setItem(3, 2, QTableWidgetItem("2236786782324"))
+        self.tableWidget.setItem(3, 0, QTableWidgetItem(""))
+        self.tableWidget.setItem(3, 1, QTableWidgetItem(""))
+        self.tableWidget.setItem(3, 2, QTableWidgetItem(""))
  
-        self.tableWidget.setItem(4, 0, QTableWidgetItem("Doe"))
-        self.tableWidget.setItem(4, 1, QTableWidgetItem("Doe@gmail.com"))
-        self.tableWidget.setItem(4, 2, QTableWidgetItem("12343445"))
+        self.tableWidget.setItem(4, 0, QTableWidgetItem(""))
+        self.tableWidget.setItem(4, 1, QTableWidgetItem(""))
+        self.tableWidget.setItem(4, 2, QTableWidgetItem(""))
  
         self.vBoxLayout = QVBoxLayout()
         self.vBoxLayout.addWidget(self.tableWidget)
