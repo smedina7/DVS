dd680a180635aa84402f68d992ffe5ac27ae78b1
bsanc114 <bsanchez12@miners.utep.edu>
2020-10-07 13:57:09 -0400

    dark theme

--- main.py
+++ main.py
@@ -3,13 +3,34 @@ import sys
 from GUI.Widgets.HomeWindow import MainGUI
 from src.fileDirectory import FileDirectory
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QMessageBox
+from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtCore import Qt
+#from PyQt5.QtGui.Qcolor
 from PyQt5.uic import loadUi
+
+
 
 
 class DVSstartUpPage(QMainWindow):
     def __init__(self):
         super(DVSstartUpPage, self).__init__()
         loadUi('GUI/src/DVSstartUpPage.ui', self)
+
+        #Dark theme
+        darkpalette = QPalette()
+        darkpalette.setColor(QPalette.Window, QColor(41,44,51))
+        darkpalette.setColor(QPalette.WindowText, Qt.white)
+        darkpalette.setColor(QPalette.Base, QColor(15,15,15))
+        darkpalette.setColor(QPalette.AlternateBase, QColor(41,44,51))
+        darkpalette.setColor(QPalette.ToolTipBase, Qt.white)
+        darkpalette.setColor(QPalette.ToolTipText, Qt.white)
+        darkpalette.setColor(QPalette.Text, Qt.white)
+        darkpalette.setColor(QPalette.Button, QColor(41,44,51))
+        darkpalette.setColor(QPalette.ButtonText, Qt.white)
+        darkpalette.setColor(QPalette.BrightText, Qt.red)
+        darkpalette.setColor(QPalette.Highlight, QColor(100,100,225))
+        darkpalette.setColor(QPalette.HighlightedText, Qt.black)
+        app.setPalette(darkpalette)
 
         self.setFixedSize(620,565)
         self.setGeometry(500, 300, 500, 100)
