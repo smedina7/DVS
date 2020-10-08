d9986e0718907252ddaaf33c9f5ea06ec7aa0f98
bsanc114 <bsanchez12@miners.utep.edu>
2020-10-08 17:31:39 -0600

    fixed dark theme again

--- main.py
+++ main.py
@@ -16,21 +16,26 @@ class DVSstartUpPage(QMainWindow):
         super(DVSstartUpPage, self).__init__()
         loadUi('GUI/src/DVSstartUpPage.ui', self)
 
-        #Dark theme
-        darkpalette = QPalette()
-        darkpalette.setColor(QPalette.Window, QColor(60,60,60)) #main window background color
-        #darkpalette.setColor(QPalette.WindowText, Qt.red) #i dont know what this is
-        darkpalette.setColor(QPalette.Base, QColor(60,60,60)) #background of the data lines
-        #darkpalette.setColor(QPalette.AlternateBase, Qt.red) # i donw know this is
-        #darkpalette.setColor(QPalette.ToolTipBase, Qt.red) # i dont know what this is
-        #darkpalette.setColor(QPalette.ToolTipText, Qt.red) # i dont know what this is
-        darkpalette.setColor(QPalette.Text, Qt.white) #text for the data lines and &drop menu
-        #darkpalette.setColor(QPalette.Button, Qt.red) # i dont know what this is for
-        darkpalette.setColor(QPalette.ButtonText, Qt.black) #text of start up buttons and tabs
-        #darkpalette.setColor(QPalette.BrightText, Qt.red) #idk
-        darkpalette.setColor(QPalette.Highlight, QColor(80,80,80)) #selected color, or highlight
-        #darkpalette.setColor(QPalette.HighlightedText, Qt.red) #selected, highlighted text color
-        app.setPalette(darkpalette)
+        QApplication.setStyle("Fusion")
+
+        # Now use a palette to switch to dark colors:
+        palette = QPalette()
+        palette.setColor(QPalette.Window, QColor(53, 53, 53))
+        palette.setColor(QPalette.WindowText, Qt.white)
+        palette.setColor(QPalette.Base, QColor(25, 25, 25))
+        palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+        palette.setColor(QPalette.ToolTipBase, Qt.white)
+        palette.setColor(QPalette.ToolTipText, Qt.white)
+        palette.setColor(QPalette.Text, Qt.white)
+        palette.setColor(QPalette.Button, QColor(53, 53, 53))
+        palette.setColor(QPalette.ButtonText, Qt.white)
+        palette.setColor(QPalette.BrightText, Qt.red)
+        palette.setColor(QPalette.Link, QColor(42, 130, 218))
+        palette.setColor(QPalette.Highlight, QColor(70,70,70))
+        palette.setColor(QPalette.HighlightedText, Qt.black)
+        QApplication.setPalette(palette)
+
+
         self.setFixedSize(620,565)
         self.setGeometry(500, 300, 500, 100)
         self.CreateNew_pushButton = self.findChild(QPushButton, 'CreateNew_pushButton')
